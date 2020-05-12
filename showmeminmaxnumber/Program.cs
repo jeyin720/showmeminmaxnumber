23ec9f003e1c6425de574f74b5793b7c92ac5bbf
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-12 12:16:04 +0900

    5개의 수를 입력받아 가장 작은수와 큰수 출력

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,27 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int max = 0, min = 0;
+            int[] num = new int[5];
+            for(int i=0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                num[i] = int.Parse(Console.ReadLine());
+                if (i == 0) min = num[i];
+                if (max < num[i])
+                {
+                    max = num[i];
+                }
+                else if(min>num[i])
+                {
+                    min = num[i];
+                }
+            }
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("가장 큰 수: " + max);
+            Console.WriteLine("가장 작은 수: " + min);
+
         }
     }
 }
